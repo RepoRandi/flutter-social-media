fd2ce00bee4e07a26bd88e169e58f528e6b9b419
Randi <randimaulanaakbar@gmail.com>
2023-03-24 14:55:02 +0700

    feat: add config date format

---
+++ lib/config/app_format.dart
@@ -0,0 +1,37 @@
+import 'package:intl/intl.dart';
+
+class AppFormat {
+  static String infoNumber(double number) {
+    return NumberFormat.compact().format(number);
+  }
+
+  static String fullDateTime(String date) {
+    return DateFormat('EEE, d MM y | HH:mm').format(DateTime.parse(date));
+  }
+
+  static publish(String date) {
+    DateTime dateTime = DateTime.parse(date);
+    DateTime now = DateTime.now();
+    DateTime yesterday = now.subtract(const Duration(days: 1));
+
+    if (dateTime.isBefore(yesterday)) {
+      return fullDateTime(date);
+    } else {
+      int hour = now.difference(dateTime).inHours;
+      if (hour >= 1) {
+        return '$hour hour';
+      } else {
+        int minute = now.difference(dateTime).inMinutes;
+        if (minute >= 1) {
+          return '$minute minute';
+        } else {
+          int second = now.difference(dateTime).inSeconds;
+          if (second >= 1) {
+            second = second < 0 ? 1 : second;
+            return '$second second';
+          }
+        }
+      }
+    }
+  }
+}
