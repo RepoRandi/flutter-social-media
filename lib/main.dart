065071a5e0b48aeb0a9967b21b4a35f5ce3f3d5f
Randi <randimaulanaakbar@gmail.com>
2023-03-25 10:43:26 +0700

    feat: add config route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:social_media/config/app_color.dart';
+import 'package:social_media/config/app_route.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +11,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
-          primaryColor: AppColor.primary,
-          colorScheme: const ColorScheme.light().copyWith(
-            primary: AppColor.primary,
-          )),
-      home: const Scaffold(),
+        primaryColor: AppColor.primary,
+        colorScheme: const ColorScheme.light().copyWith(
+          primary: AppColor.primary,
+        ),
+      ),
+      routerConfig: AppRoute.routerConfig,
     );
   }
 }
