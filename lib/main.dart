4a1fdf62144f224b9b197ec455ce814d8ac52f9c
Randi <randimaulanaakbar@gmail.com>
2023-03-26 15:06:52 +0700

    feat: add controller home

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:social_media/config/app_color.dart';
 import 'package:social_media/config/app_route.dart';
+import 'package:social_media/controller/c_home.dart';
 import 'package:social_media/controller/c_user.dart';
 
 void main() {
@@ -18,6 +19,9 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(
           create: (_) => CUser(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => CHome(),
         )
       ],
       child: MaterialApp.router(
